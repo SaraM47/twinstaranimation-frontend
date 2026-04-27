6460c13dd92688aad17246f6559394cddf97b233
SaraMsoso <Sara.soder44@gmail.com>
2026-04-27 11:43:44 +0200

    Configured API proxy and cookie forwarding for backend auth

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+
+  server: {
+    port: 5174,
+
+    proxy: {
+      "/api": {
+        target: "http://localhost:5066", // backend
+        changeOrigin: true,
+        secure: false,
+
+        // important to handle cookies correctly
+        cookieDomainRewrite: "localhost",
+
+        // forward cookies from client to backend
+        configure: (proxy) => {
+          proxy.on("proxyReq", (proxyReq, req) => {
+            if (req.headers.cookie) {
+              proxyReq.setHeader("cookie", req.headers.cookie);
+            }
+          });
+        },
+      },
+    },
+  },
+});
