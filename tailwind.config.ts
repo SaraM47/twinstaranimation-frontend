ca90adac32b3a416c51eb2dbee7c0246461295f0
SaraMsoso <Sara.soder44@gmail.com>
2026-04-27 11:43:35 +0200

    Added custom heading and body font families

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+    theme: {
+      extend: {
+        fontFamily: {
+          heading: ['"Noto Serif JP"', 'serif'],
+          body: ['"M PLUS 2"', 'sans-serif'],
+        },
+      },
+    },
+  };
