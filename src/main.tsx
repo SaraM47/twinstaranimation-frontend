b15272d4d68d900759c1b31c19fd00b044d0aa5a
SaraMsoso <Sara.soder44@gmail.com>
2026-04-27 11:44:17 +0200

    Setup QueryClient, providers, router, and global toast system

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./app/router/routes";
+import AppProviders from "./app/providers/AppProviders";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <AppProviders>
+        <RouterProvider router={router} />
+
+        {/* Global toast system */}
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            style: {
+              background: "#000",
+              color: "#fff",
+              border: "1px solid rgba(255,255,255,0.1)",
+            },
+          }}
+        />
+      </AppProviders>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
