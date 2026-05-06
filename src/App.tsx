827550def4c7073ce26f1224b3b4608e08246f09
SaraMsoso <Sara.soder44@gmail.com>
2026-05-06 12:37:46 +0200

    Wrapped auth routes with PublicLayout for shared UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,33 @@ import LoginPage from "./features/auth/pages/LoginPage";
 import CreatorDashboardPage from "./features/creator/pages/CreatorDashboardPage";
 import ProtectedRoute from "./app/router/guards/ProtectedRoute";
 import RoleRoute from "./app/router/guards/RoleRoute";
+import PublicLayout from "./components/layout/PublicLayout";
+import RegisterPage from "./features/auth/pages/RegisterPage";
 
 function App() {
   return (
-    <Routes>
-      {/* Public */}
-      <Route path="/login" element={<LoginPage />} />
+    <>
+      <Routes>
+        {/* Public */}
+        <Route element={<PublicLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<LoginPage />} />
+        </Route>
 
-      {/* Protected */}
-      <Route
-        path="/creator/dashboard"
-        element={
-          <ProtectedRoute>
-            <RoleRoute role="Creator">
-              <CreatorDashboardPage />
-            </RoleRoute>
-          </ProtectedRoute>
-        }
-      />
-
-      {/* Default */}
-      <Route path="*" element={<LoginPage />} />
-    </Routes>
+        {/* Protected */}
+        <Route
+          path="/creator/dashboard"
+          element={
+            <ProtectedRoute>
+              <RoleRoute role="Creator">
+                <CreatorDashboardPage />
+              </RoleRoute>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
   );
 }
 
